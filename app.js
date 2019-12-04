d8bbadb212e25ea7ef660f19ffb00540c4444f7f
eparionad <eparionad@gmail.com>
2019-12-03 21:04:51 -0500

    se agrego el evento key

--- app.js
+++ app.js
@@ -26,3 +26,19 @@ ar.forEach(function(numero){
         var sonaraudio = document.getElementById(nombreaudio).play();
     });
 });
+
+
+
+//Piano con evento tecla
+var entrada1 = document.getElementsByTagName('body')[0];
+entrada1.addEventListener('keydown',function(e){
+    var letra1 = e.key;
+
+    if(letra1 !== '1' && letra1 !== '2' && letra1 !== '3' && letra1 !== '4' && letra1 !== '5' && letra1 !== '6' && letra1 !== '7'){
+        alert('no es la tecla, las teclas son: 1,2,3,4,5,6,7');
+    }else{
+        var dd = document.querySelector(`audio[data-key="${letra1}"]`).pause();
+        var dd = document.querySelector(`audio[data-key="${letra1}"]`).currentTime=0;
+        var dd = document.querySelector(`audio[data-key="${letra1}"]`).play();
+    }
+});
